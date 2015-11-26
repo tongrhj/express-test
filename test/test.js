e8247af3e188f80c6f0581a95ee50008b2dce0af
tongrhj <tongrhj@gmail.com>
2015-11-26 13:50:30 +0800

    Completed the express challenges

---
+++ test/test.js
@@ -0,0 +1,22 @@
+'use strict'
+var expect = require('chai').expect
+
+describe('Server', () => {
+
+    // describe('JSON Data', () => {
+    //
+    //   it('should respond with JSON data', () => {
+    //     var sampleData = server
+    //     expect(sampleData).to.be.true
+    //   })
+    //
+    // })
+
+})
+
+// Server
+// should respond with JSON data
+// should provide a list of all class participants
+// should allow searching by name
+// should allow sorting alphabetically by name
+// should allow whitelisting specific fields
